7b6b61921c263707f5a08c6ae28c4d03de77502b
kvartborg <hello@kvartborg.net>
2020-03-22 13:48:00 +0100

    Add test to user controller

--- model/migrate.go
+++ model/migrate.go
@@ -24,11 +24,11 @@ var tables = []interface{}{
 }
 
 func Migrate() {
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 	db.Migrate(tables...)
 }
 
 func Reset() {
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 	db.Reset(tables...)
 }
