ad1fd63f5eebcbb27017f7d39f5a276224d10f29
NJohansen <niclas.w.j@hotmail.dk>
2020-03-23 22:00:52 +0100

    Added routes for programs

--- main.go
+++ main.go
@@ -5,17 +5,24 @@ import (
 	"net/http"
 
 	"github.com/TV2-Bachelorproject/server/controller/people"
+	"github.com/TV2-Bachelorproject/server/controller/programs"
 	"github.com/TV2-Bachelorproject/server/model"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func routes(r *mux.Router) {
+
+	//Routes for people
 	r.HandleFunc("/people", people.List).Methods("GET")
 	r.HandleFunc("/people", people.Create).Methods("POST")
 	r.HandleFunc("/people/{id:[0-9]+}", people.Show).Methods("GET")
 	r.HandleFunc("/people/{id:[0-9]+}", people.Update).Methods("PUT")
 	r.HandleFunc("/people/{id:[0-9]+}", people.Delete).Methods("DELETE")
+
+	//Routes for programs
+	r.HandleFunc("/programs", programs.GetAll).Methods("GET")
+	r.HandleFunc("/programs/{id:[0-9]+}", programs.Get).Methods("GET")
 }
 
 func main() {
