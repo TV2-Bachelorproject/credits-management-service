9108b8dcdc008738493961ec3bfc4b72380446ce
NJohansen <niclas.w.j@hotmail.dk>
2020-03-23 22:03:34 +0100

    Fixed preloading of structs

--- model/public/program.go
+++ model/public/program.go
@@ -1,10 +1,13 @@
 package public
 
 import (
+	"fmt"
+
 	"github.com/TV2-Bachelorproject/server/pkg/db"
 	"github.com/jinzhu/gorm"
 )
 
+//Program struct
 type Program struct {
 	gorm.Model
 	ProgramID           string `json:"programId"`
@@ -13,29 +16,49 @@ type Program struct {
 	Description         string
 	Cast                string
 	CategoryID          uint
-	Category            Category
-	Genres              []Genre `gorm:"many2many:genre_programs;"`
+	Category            Category `gorm:"foreignkey:CategoryID"`
+	Genres              []Genre  `gorm:"many2many:genre_programs;"`
 	SeasonID            uint
-	Season              Season
+	Season              Season `gorm:"foreignkey:SeasonID"`
 	SeasonEpisodeNumber int
 	LinearEpisodeNumber int
 	ProductionID        uint
-	Production          Production
+	Production          Production `gorm:"foreignkey:ProductionID"`
 	SerieID             uint
-	Serie               Serie
-	AirtimeFrom         int `gorm:"type:bigint"`
-	AirtimeTo           int `gorm:"type:bigint"`
-	Credit              []Credits
+	Serie               Serie     `gorm:"foreignkey:SerieID"`
+	AirtimeFrom         int       `gorm:"type:bigint"`
+	AirtimeTo           int       `gorm:"type:bigint"`
+	Credit              []Credits `gorm:"many2many:credit_groups;"`
 }
 
+//Find single program entry
 func (p Program) Find(id uint) Program {
-	db.Model(p).Where("id = ?", id).First(&p)
+	//Preload preloads structs
+	if err := db.Model(p).
+		Preload("Production").
+		Preload("Category").
+		Preload("Genres").
+		Preload("Serie").
+		Preload("Season").
+		Preload("Credit").
+		Where("id = ?", id).
+		First(&p).Error; err != nil {
+		fmt.Println(err)
+	}
 	return p
 }
 
+//Programs struct
 type Programs []Program
 
+//Find all programs
 func (p Programs) Find() Programs {
-	db.Model(&Program{}).Find(&p)
+	db.Model(&Program{}).
+		Preload("Production").
+		Preload("Category").
+		Preload("Genres").
+		Preload("Serie").
+		Preload("Season").
+		Find(&p).GetErrors()
 	return p
 }
