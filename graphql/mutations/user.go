397cff557c1f2f109a225bb18f0e2abc8ebc3c28
NJohansen <niclas.w.j@hotmail.dk>
2020-04-07 19:53:22 +0200

    mutation for user added

---
+++ graphql/mutations/user.go
@@ -0,0 +1,75 @@
+package mutations
+
+import (
+	"fmt"
+
+	"github.com/TV2-Bachelorproject/server/model/user"
+	"github.com/TV2-Bachelorproject/server/pkg/db"
+	"github.com/graphql-go/graphql"
+)
+
+/*
+	Mutation object type with fields "createUser" has type UserType:
+       - Name: Mutation
+       - Fields: a map of fields by using graphql.Fields
+   Setup type of field, args and resolver function:
+       - Type: user
+       - Args: name,email,password,type
+       - Resolve: function to create user
+*/
+var UserType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"createUser": &graphql.Field{
+			Type: user.UserType,
+			Description: "Create new User",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"email": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				}, 
+				"password": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"type": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error){
+				u := user.User{}
+				theType, ok := params.Args["type"].(int)
+
+				//if admin
+				if ok && theType == 1 {
+					//create user
+					u, _ = user.New(
+						params.Args["name"].(string), 
+						params.Args["email"].(string),
+						params.Args["password"].(string),
+						user.Producer)
+	
+					db.Create(&u)
+					return u,nil
+				}
+
+				//if producer
+				if ok && theType == 0 {
+					//create user
+					u, _ = user.New(
+						params.Args["name"].(string), 
+						params.Args["email"].(string),
+						params.Args["password"].(string),
+						user.Admin)
+	
+					db.Create(&u)
+					return u,nil
+				}
+	
+			return nil, nil
+
+			},
+		},
+	},
+})
